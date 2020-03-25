b5c830bf72c4d9e944fea78ef35b7a1e4fa9ddc5
Maruneac Cosmin <cosmin.maruneac@raisissoftware.ro>
2020-03-25 17:02:48 +0200

    Laboratorul 1

---
+++ Farmacie/Medicament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Farmacie
+{
+    class Medicament
+    {
+        int[] pret; // data membra privata
+        public string nume_med;
+        public string data_exp;
+
+        public Medicament()
+        {
+            nume_med = data_exp = string.Empty;
+
+        }
+        public Medicament(string _nume_med, string _data_exp, int[] _pret)
+        {
+            nume_med = _nume_med;
+            data_exp = _data_exp;
+            _pret.CopyTo(pret, 0);
+        }
+
+    }
+}
---
+++ Farmacie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Farmacie;
+
+namespace Farmacie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Medicament t;
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
