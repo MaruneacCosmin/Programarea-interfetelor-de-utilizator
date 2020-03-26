63f2fc794218323f2b1ea73ee04a6ef53433bad8
Maruneac Cosmin <cosmin.maruneac@raisissoftware.ro>
2020-03-26 19:08:30 +0200

    Lab03

--- Farmacie/Medicament.cs
+++ Farmacie/Medicament.cs
@@ -5,21 +5,43 @@ namespace Farmacie
 {
     class Medicament
     {
-        int[] pret; // data membra privata
+        int pret; // data membra privata
         public string nume_med;
         public string data_exp;
+        public int cantitate;
 
         public Medicament()
         {
             nume_med = data_exp = string.Empty;
+            pret = 0;
+            cantitate = 0;
            
         }
-        public Medicament(string _nume_med, string _data_exp, int[] _pret)
+        public Medicament(string _nume_med, string _data_exp, int _pret, int _cantitate)
         {
             nume_med = _nume_med;
             data_exp = _data_exp;
-            _pret.CopyTo(pret, 0);
+            pret = _pret;
+            cantitate = _cantitate;
         }
 
+        public void SetDate(string date)
+        {
+            string[] _date = date.Split(' ');
+            nume_med = _date[0];
+            data_exp = _date[1];
+            cantitate = Convert.ToInt32(_date[2]);
+            pret = Convert.ToInt32(_date[3]);
+        }
+        public string ConversieLaSir()
+        {
+            string sDate = "Nu exista (Nu ati apelat metoda **SetDate**)";
+            if ( nume_med != null)
+            {
+                sDate = string.Join(", ",nume_med, data_exp, cantitate, pret );
+            }
+            string s = string.Format(" {0} ", sDate);
+            return s;
+        }
     }
 }
--- Farmacie/Program.cs
+++ Farmacie/Program.cs
@@ -7,8 +7,44 @@ namespace Farmacie
     {
         static void Main(string[] args)
         {
-            Medicament t;
-            Console.ReadKey();
+            Medicament t = new Medicament("Parasinus", "24.04.2019", 24, 10);
+            string opt;
+            bool ok = true;
+            while (ok == true)
+            {
+                
+                Console.WriteLine("A: Adaugare medicament "); // Implemnetat doar pentru cerinta laboratorului.
+                Console.WriteLine("E: Editare medicament");
+                Console.WriteLine("D: Stergere medicament");
+                Console.WriteLine("O: Afisare lista medicamente"); // urmeaza a fi implementata ca lista. Pentru moment am respectat doar cerinta laboratorului.
+                Console.WriteLine("S: Cautare medicamente");
+                Console.WriteLine("X: Iesire");
+                opt = Console.ReadLine();
+                switch (opt.ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine("Adaugati informatiile despre medicament");
+                        t.SetDate(Console.ReadLine());
+                        break;
+                    case "E":
+                        {
+                            break;
+                        }
+                       
+                    case "D":
+                        break;
+                    case "O":
+                        Console.WriteLine(t.ConversieLaSir());
+                        break;
+                    case "S":
+                        
+                        break;
+                    case "X":
+                        ok = false;
+                        break;
+                }
+
+            }
             Console.ReadKey();
         }
     }
